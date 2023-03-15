fe46bc72d80d462a36a883b99c5e312953a57273
Pallav Jain <pallavjain12@gmail.com>
2023-03-15 19:54:22 +0530

    Refactored Code

--- app/src/main/java/com/example/codetantraproxy/AddEmailPassword.java
+++ app/src/main/java/com/example/codetantraproxy/AddEmailPassword.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -58,64 +57,55 @@ public class AddEmailPassword extends AppCompatActivity {
             chbx.setText(em.getValue());
             chbx.setId(em.getKey());
             linearLayoutStudents.addView(chbx);
-            chbx.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                    if (isChecked) {
-                        idsToRemove.add(em.getKey());
-                    }
-                    else {
-                        idsToRemove.remove(Integer.valueOf(em.getKey()));
-                    }
+            chbx.setOnCheckedChangeListener((compoundButton, isChecked) -> {
+                if (isChecked) {
+                    idsToRemove.add(em.getKey());
+                }
+                else {
+                    idsToRemove.remove(em.getKey());
                 }
             });
         }
 
-        btnDeleteAccounts.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                for (int i : idsToRemove) {
-                    if (i == 1) continue;
-                    if (deleteFromDB(getApplicationContext(), i)) {
-                        CheckBox chbx = new CheckBox(getApplicationContext());
-                        chbx.setId(i);
-                        linearLayoutStudents.removeView(chbx);
-                        Toast.makeText(getApplicationContext(), "Deleted Successfully", Toast.LENGTH_LONG).show();
-                    }
-                    else {
-                        Toast.makeText(getApplicationContext(), "Error deleting", Toast.LENGTH_LONG).show();
-                    }
-                }
-                finish();
-                startActivity(getIntent());
-            }
-        });
-
-        addUserButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email = emailEditText.getText().toString();
-                String password = passwordEditText.getText().toString();
-                password = convertToURLSafe(password);
-                if (checkCredentials(email, password)) {
-                    Toast.makeText(getApplicationContext(), "Credentials verified successfully", Toast.LENGTH_LONG).show();
-                    try {
-                        DatabaseHelper databaseHelper = DatabaseHelper.getDB(getApplicationContext());
-                        databaseHelper.userDao().addUser(new User(email, password));
-                        Toast.makeText(getApplicationContext(), "User added successfully", Toast.LENGTH_LONG).show();
-                    }
-                    catch (Exception e) {
-                        Toast.makeText(getApplicationContext(), "Error occured while adding user. Please try again later", Toast.LENGTH_LONG).show();
-                    }
-                    emailEditText.setText("");
-                    passwordEditText.setText("");
+        btnDeleteAccounts.setOnClickListener(view -> {
+            for (int i : idsToRemove) {
+                if (i == 1) continue;
+                if (deleteFromDB(getApplicationContext(), i)) {
+                    CheckBox chbx = new CheckBox(getApplicationContext());
+                    chbx.setId(i);
+                    linearLayoutStudents.removeView(chbx);
+                    Toast.makeText(getApplicationContext(), "Deleted Successfully", Toast.LENGTH_LONG).show();
                 }
                 else {
-                    Toast.makeText(getApplicationContext(), "Incorrect email or password", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Error deleting", Toast.LENGTH_LONG).show();
                 }
-                finish();
-                startActivity(getIntent());
             }
+            finish();
+            startActivity(getIntent());
+        });
+
+        addUserButton.setOnClickListener(view -> {
+            String email = emailEditText.getText().toString();
+            String password = passwordEditText.getText().toString();
+            password = convertToURLSafe(password);
+            if (checkCredentials(email, password)) {
+                Toast.makeText(getApplicationContext(), "Credentials verified successfully", Toast.LENGTH_LONG).show();
+                try {
+                    DatabaseHelper databaseHelper = DatabaseHelper.getDB(getApplicationContext());
+                    databaseHelper.userDao().addUser(new User(email, password));
+                    Toast.makeText(getApplicationContext(), "User added successfully", Toast.LENGTH_LONG).show();
+                }
+                catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "Error occured while adding user. Please try again later", Toast.LENGTH_LONG).show();
+                }
+                emailEditText.setText("");
+                passwordEditText.setText("");
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "Incorrect email or password", Toast.LENGTH_LONG).show();
+            }
+            finish();
+            startActivity(getIntent());
         });
     }
 }
